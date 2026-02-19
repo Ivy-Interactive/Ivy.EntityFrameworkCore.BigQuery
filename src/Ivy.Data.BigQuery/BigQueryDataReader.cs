19ed91fcc68c0783f88034b65add8abf324ae6d4
Dovydas M <dovydas.m@gmail.com>
2026-02-19 18:21:46 +0200

    Added ADO.NET NTS plugin project and package (#17)

--- src/Ivy.Data.BigQuery/BigQueryDataReader.cs
+++ src/Ivy.Data.BigQuery/BigQueryDataReader.cs
@@ -5,15 +5,14 @@ using Google.Apis.Bigquery.v2.Data;
 using System.Globalization;
 using System.Collections;
 using System.Collections.ObjectModel;
-using NetTopologySuite.Geometries;
-using NetTopologySuite.IO;
+
 
 namespace Ivy.Data.BigQuery
 {
     public class BigQueryDataReader : DbDataReader, IDbColumnSchemaGenerator
     {
         private readonly BigQueryResults _results;
-        private readonly BigQueryClient _client; //todo remove?
+        private readonly BigQueryClient _client;
         private readonly TableSchema _schema;
         private readonly Dictionary<string, int> _fieldNameLookup;
         private readonly Type[] _fieldTypes;
@@ -514,33 +513,12 @@ namespace Ivy.Data.BigQuery
                     return (T)(object)bytesValue;
                 }
 
-                // GEOGRAPHY
-                if (typeof(Geometry).IsAssignableFrom(typeof(T)))
+                // GEOGRAPHY - use plugin handler if available
+                if (BigQueryTypeHandlers.GeographyHandler is { } geoHandler && geoHandler.IsGeographyType(typeof(T)))
                 {
-                    Geometry? geometry = null;
-
-                    if (value is BigQueryGeography bqGeography)
+                    if (geoHandler.TryConvertFromGeography(value, typeof(T), out var geometry) && geometry != null)
                     {
-                        // BigQueryGeography contains WKT in Text property
-                        var wktReader = new WKTReader();
-                        geometry = wktReader.Read(bqGeography.Text);
-                    }
-                    else if (value is string stringValue)
-                    {
-                        // Direct WKT string
-                        var wktReader = new WKTReader();
-                        geometry = wktReader.Read(stringValue);
-                    }
-                    else if (value is IDictionary<string, object> dict)
-                    {
-                        // Simple: {"Text":"POINT(0 0)","SRID":4326}
-                        // Collection: {"Geometries":[{"Text":"LINESTRING(...)"},...],"SRID":4326}
-                        geometry = ParseBigQueryGeography(dict);
-                    }
-
-                    if (geometry != null)
-                    {
-                        return (T)(object)geometry;
+                        return (T)geometry;
                     }
 
                     string valueJson;
@@ -952,101 +930,5 @@ namespace Ivy.Data.BigQuery
             return isArray ? Array.CreateInstance(baseType, 0).GetType() : baseType;
         }
 
-        /// <summary>
-        /// Parses BigQuery's custom geography dictionary format into NTS Geometry.
-        /// BigQuery format:
-        /// - Simple: {"Text":"POINT(0 0)","SRID":4326}
-        /// - Collection: {"Geometries":[{"Text":"LINESTRING(...)"},...],"SRID":4326}
-        /// </summary>
-        private static Geometry? ParseBigQueryGeography(IDictionary<string, object> dict)
-        {
-            var wktReader = new WKTReader();
-
-            if (dict.TryGetValue("Text", out var textObj) && textObj is string wkt)
-            {
-                var geometry = wktReader.Read(wkt);
-
-                if (dict.TryGetValue("SRID", out var sridObj))
-                {
-                    var srid = Convert.ToInt32(sridObj);
-                    geometry.SRID = srid;
-                }
-
-                return geometry;
-            }
-
-            if (dict.TryGetValue("Geometries", out var geometriesObj))
-            {
-                IEnumerable<object>? geometriesEnumerable = null;
-
-                if (geometriesObj is object[] arr)
-                    geometriesEnumerable = arr;
-                else if (geometriesObj is IList list)
-                    geometriesEnumerable = list.Cast<object>();
-                else if (geometriesObj is IEnumerable enumerable && geometriesObj is not string)
-                    geometriesEnumerable = enumerable.Cast<object>();
-
-                if (geometriesEnumerable == null)
-                {
-                    throw new InvalidCastException($"Geometries property has unexpected type: {geometriesObj?.GetType().FullName}");
-                }
-
-                var geometriesArray = geometriesEnumerable.ToArray();
-                var geometries = new List<Geometry>();
-
-                foreach (var item in geometriesArray)
-                {
-                    if (item is IDictionary<string, object> geomDict)
-                    {
-                        var geom = ParseBigQueryGeography(geomDict);
-                        if (geom != null)
-                        {
-                            geometries.Add(geom);
-                        }
-                    }
-                    else if (item is BigQueryGeography bqGeom)
-                    {
-                        var geom = wktReader.Read(bqGeom.Text);
-                        geometries.Add(geom);
-                    }
-                    else
-                    {
-                        throw new InvalidCastException($"Geometry item has unexpected type: {item?.GetType().FullName}. Value: {item}");
-                    }
-                }
-
-                if (geometries.Count == 0)
-                {
-                    throw new InvalidCastException($"No geometries parsed from array of {geometriesArray.Length} items");
-                }
-
-                var srid = 0;
-                if (dict.TryGetValue("SRID", out var collectionSridObj))
-                {
-                    srid = Convert.ToInt32(collectionSridObj);
-                }
-
-                var factory = new GeometryFactory(new PrecisionModel(), srid);
-
-                var firstGeom = geometries[0];
-                if (geometries.All(g => g is Point))
-                {
-                    return factory.CreateMultiPoint(geometries.Cast<Point>().ToArray());
-                }
-                if (geometries.All(g => g is LineString))
-                {
-                    return factory.CreateMultiLineString(geometries.Cast<LineString>().ToArray());
-                }
-                if (geometries.All(g => g is Polygon))
-                {
-                    return factory.CreateMultiPolygon(geometries.Cast<Polygon>().ToArray());
-                }
-
-                // Mixed types
-                return factory.CreateGeometryCollection(geometries.ToArray());
-            }
-
-            return null;
-        }
     }
 }
